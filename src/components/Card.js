6b6c312717f9d0f20a224e74236818c58454d3d5
AshishPku <2022uec1698@mnit.ac.in>
2024-06-16 12:07:36 +0530

    Finally learn state in react which two things one is variable and other is function which is changing the value of the variable the syntax is const [x,y]=useState(1) Here x is variable which has value equal to 1 initially and y is function which can be call anywhere inside the components to change the value of the variable x.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 const data = [
   "Job 1 is amazing but it's hard to crack,🙋",
   "Job 2 is standard but it's can be crack.🙋‍♂️",
   "Job 3 is good can be cracked.🙋‍♂️",
 ];
 const Card = () => {
-  var step = 1;
+  const [step, set] = useState(0);
   const handlePrevious = () => {
-    alert("previous");
+    set((step) => {
+      if (step > 0) {
+        return step - 1;
+      } else {
+        return 0;
+      }
+    });
   };
   const handleNext = () => {
-    alert("Next");
+    set((step) => {
+      if (step < 2) {
+        return step + 1;
+      } else {
+        return 2;
+      }
+    });
   };
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`num ${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`num ${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`num ${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={`num ${step >= 0 ? "active" : ""}`}>1</div>
+        <div className={`num ${step >= 1 ? "active" : ""}`}>2</div>
+        <div className={`num ${step >= 2 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">{data[step - 1]}</p>
+      <p className="message">{data[step]}</p>
       <div className="button">
         <button onClick={handlePrevious}>Previous</button>
         <button onClick={handleNext}>Next</button>
